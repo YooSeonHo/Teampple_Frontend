fc433ddf505311137afc8c243f4a3fc74cd95c9a
임채리 <limch9eri@google.com>
2023-01-07 17:18:23 +0900

    feat: 추가정보 페이지

--- src/components/moreInfoPage/InfoInput.tsx
+++ src/components/moreInfoPage/InfoInput.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const InfoInput = () => {
+  const [name, setName] = useState('');
+  const [school, setSchool] = useState('');
+  const [depart, setDepart] = useState('');
+
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const onChangeSchool = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSchool(e.target.value);
+  };
+  const onChangeDepart = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDepart(e.target.value);
+  };
+
   return (
     <div>
       <InputContainer>
         <InputBox>
           <PlaceHolder>이름</PlaceHolder>
-          <Input />
+          <Input onChange={onChangeName} />
         </InputBox>
         <InputBox>
           <PlaceHolder>학교</PlaceHolder>
-          <Input />
+          <Input onChange={onChangeSchool} />
         </InputBox>
         <InputBox>
           <PlaceHolder>학과</PlaceHolder>
-          <Input />
+          <Input onChange={onChangeDepart} />
         </InputBox>
       </InputContainer>
+      <Btn
+        disabled={
+          name.length !== 0 && school.length !== 0 && depart.length !== 0
+            ? false
+            : true
+        }
+      >
+        <Text>팀쁠 시작하기</Text>
+      </Btn>
     </div>
   );
 };
@@ -37,6 +60,7 @@ const InputBox = styled.div`
 `;
 
 const InputContainer = styled.div`
+  margin-bottom: 40px;
 `;
 const PlaceHolder = styled.span`
   margin-right: 24px;
@@ -44,11 +68,26 @@ const PlaceHolder = styled.span`
   font-size: 20px;
 `;
 
-
 const Input = styled.input`
   border: none;
   font-size: 20px;
   width: 270px;
+`;
+
+const Btn = styled.button`
+  width: 372px;
+  height: 54px;
+  background-color: #487aff;
+  border-radius: 8px;
+  &:disabled {
+    background-color: #d4e4ff;
+  }
+`;
+
+const Text = styled.span`
+  font-size: 20px;
+  line-height: 100%;
+  color: #ffffff; ;
 `;
 
 export default InfoInput;
--- src/pages/MoreInfoPage.tsx
+++ src/pages/MoreInfoPage.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
-import StartBtn from 'components/moreInfoPage/StartBtn';
 import InfoInput from 'components/moreInfoPage/InfoInput';
+import Logo from '../components/images/Logo_login.png';
 
 const MoreInfoPage = () => {
   return (
     <MoreInfoPageContainer>
-      <InfoInput />
-      <StartBtn />
+      <LogoImg src={Logo}></LogoImg>
+      <InputBox>
+        <InfoInput />
+      </InputBox>
     </MoreInfoPageContainer>
   );
 };
@@ -16,6 +18,22 @@ const MoreInfoPageContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: #f4f8ff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+const LogoImg = styled.img`
+  width: 237.26px;
+  height: 67px;
+  position: absolute;
+  top: 219px;
+`;
+
+const InputBox = styled.div`
+  position: absolute;
+  top: 357px;
 `;
 
 export default MoreInfoPage;
