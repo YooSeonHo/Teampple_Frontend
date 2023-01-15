e414106f11a67f1f698ace01be89f7348019052d
임채리 <limch9eri@google.com>
2023-01-16 00:20:20 +0900

    chore: prettier 적용

--- src/components/filePage/fileList.tsx
+++ src/components/filePage/fileList.tsx
@@ -1,21 +1,20 @@
-import styled from "styled-components";
-import React, { useEffect, useState } from "react";
+import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
 import arrow from '../images/ArrowLineRight2.png';
-import axios from "axios";
-import { FileInfo } from "interfaces";
-import { useRecoilState } from "recoil";
-import { IsSearchState, searchFileState } from "state";
+import axios from 'axios';
+import { FileInfo } from 'interfaces';
+import { useRecoilState } from 'recoil';
+import { IsSearchState, searchFileState } from 'state';
 
 const ListBox = styled.div`
+  width: 1172px;
+  height: 810px;
+  display: flex;
+  flex-direction: column;
+  margin-left: auto;
+  margin-right: auto;
 
-    width: 1172px;
-    height: 810px;
-    display: flex;
-    flex-direction: column;
-    margin-left : auto;
-    margin-right : auto;
-
-.fileHeader{
+  .fileHeader {
     font-weight: 500;
     font-size: 18px;
     line-height: 100%;
@@ -23,46 +22,45 @@ const ListBox = styled.div`
     display: flex;
     margin-top: 20px;
     height: 52px;
-}
+  }
 
-.fileName{
+  .fileName {
     width: 400px;
-}
+  }
 
-.fileLoc{
+  .fileLoc {
     width: 200px;
-}
+  }
 
-.fileOwner{
+  .fileOwner {
     width: 200px;
-}
+  }
 
-.fileSize{
+  .fileSize {
     width: 100px;
-}
+  }
 
-.fileDate{
+  .fileDate {
     width: 200px;
-}
+  }
 
-.fileList{
+  .fileList {
     display: flex;
     flex-direction: column;
-}`;
-
+  }
+`;
 
 const File = styled.div`
+  display: flex;
+  width: 1172px;
+  height: 72px;
 
-    display: flex;
-    width: 1172px;
-    height: 72px;
-
-    :hover{
-    background-color: #EAF2FF;
+  :hover {
+    background-color: #eaf2ff;
     cursor: grab;
-    }
+  }
 
-.name{
+  .name {
     width: 376px;
     font-weight: 500;
     font-size: 18px;
@@ -72,155 +70,148 @@ const File = styled.div`
     margin-top: auto;
     margin-bottom: auto;
     margin-left: 24px;
-}
+  }
 
-.loc{
+  .loc {
     width: 200px;
     font-weight: 500;
     font-size: 16px;
     line-height: 100%;
-    color: #A7A7A7;
+    color: #a7a7a7;
     max-width: 200px;
     margin-top: auto;
     margin-bottom: auto;
-}
+  }
 
-.owner,
-.date{
+  .owner,
+  .date {
     font-weight: 400;
     font-size: 16px;
     line-height: 100%;
-    color: #A7A7A7;
+    color: #a7a7a7;
     width: 200px;
     max-width: 200px;
     margin-top: auto;
     margin-bottom: auto;
-}
+  }
 
-.size{
+  .size {
     width: 100px;
     max-width: 100px;
     font-weight: 400;
     font-size: 16px;
     line-height: 100%;
-    color: #A7A7A7;
+    color: #a7a7a7;
     margin-top: auto;
     margin-bottom: auto;
-}
+  }
 
-.icon{
+  .icon {
     height: 16px;
     width: 15px;
     margin-top: auto;
     margin-bottom: auto;
     margin-left: auto;
     margin-right: 29px;
-}
+  }
 
-.icon:hover{
+  .icon:hover {
     cursor: grab;
-}
+  }
 
-img{
+  img {
     max-width: 100%;
     max-height: 100%;
-}
+  }
 `;
 
+const FileList = () => {
+  const [files, setFiles] = useState([]);
+  const [searchFile, setSearchFile] = useRecoilState(searchFileState);
+  const [isSearch, setIsSearch] = useRecoilState(IsSearchState);
 
-const FileList = () =>{
-    const [files,setFiles] = useState([]);
-    const [searchFile,setSearchFile] = useRecoilState(searchFileState);
-    const [isSearch,setIsSearch] = useRecoilState(IsSearchState);
+  useEffect(() => {
+    const getFiles = async () => {
+      await axios({
+        url: `/api/files`,
+        baseURL: 'https://www.teampple.site/',
+        method: 'get',
+        params: {
+          teamId: 1,
+        },
+        //파람스 부분 바꾸면 댐
+      })
+        .then((res) => {
+          setFiles(res.data.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    };
 
-    
-    useEffect(()=>{
+    getFiles();
+  }, []);
 
-        const getFiles = async () =>{
-            await axios({
-                url : `/api/files`,
-                baseURL : "https://www.teampple.site/",
-                method : 'get',
-                params: {
-                    teamId: 1
-                }
-                //파람스 부분 바꾸면 댐
-            })
-            .then((res)=>{
-                setFiles(res.data.data);
-            })
-            .catch((e)=>{
-                console.log(e)
-            })
-        }
-
-        getFiles()
-    },[])
-
-    return (
-      <ListBox>
-        <div className="fileHeader">
-          <div className="fileName">파일명</div>
-          <div className="fileLoc">파일 경로</div>
-          <div className="fileOwner">파일 업로더</div>
-          <div className="fileSize">크기</div>
-          <div className="fileDate">날짜</div>
-        </div>
-        <div className="fileList">
-          {files && isSearch
-            ? files
-                .filter((file: FileInfo) => {
-                  return file.fileName
-                    .toLowerCase()
-                    .includes(searchFile.toLowerCase());
-                })
-                .map((file: FileInfo) => {
-                  return (
-                    <File key={file.updatedAt}>
-                      <div className="name">{file.fileName}</div>
-                      <div className="loc">{file.url}</div>
-                      <div className="owner">{file.uploader}</div>
-                      <div className="size">
-                        {Math.round(file.size / 1024)}MB
-                      </div>
-                      <div className="date">
-                        {file.updatedAt
-                          .replace(/-/g, '.')
-                          .replace('T', ' ')
-                          .replace(/:[0-9]+$/, '')}
-                      </div>
-                      <div className="icon">
-                        <img src={arrow} />
-                      </div>
-                    </File>
-                  );
-                })
-            : files &&
-              files.map(
-                (file: FileInfo) =>
-                  file && (
-                    <File key={file.updatedAt}>
-                      <div className="name">{file.fileName}</div>
-                      <div className="loc">{file.url}</div>
-                      <div className="owner">{file.uploader}</div>
-                      <div className="size">
-                        {Math.round(file.size / 1024)}MB
-                      </div>
-                      <div className="date">
-                        {file.updatedAt
-                          .replace(/-/g, '.')
-                          .replace('T', ' ')
-                          .replace(/:[0-9]+$/, '')}
-                      </div>
-                      <div className="icon">
-                        <img src={arrow} />
-                      </div>
-                    </File>
-                  ),
-              )}
-        </div>
-      </ListBox>
-    );
-}
+  return (
+    <ListBox>
+      <div className="fileHeader">
+        <div className="fileName">파일명</div>
+        <div className="fileLoc">파일 경로</div>
+        <div className="fileOwner">파일 업로더</div>
+        <div className="fileSize">크기</div>
+        <div className="fileDate">날짜</div>
+      </div>
+      <div className="fileList">
+        {files && isSearch
+          ? files
+              .filter((file: FileInfo) => {
+                return file.fileName
+                  .toLowerCase()
+                  .includes(searchFile.toLowerCase());
+              })
+              .map((file: FileInfo) => {
+                return (
+                  <File key={file.updatedAt}>
+                    <div className="name">{file.fileName}</div>
+                    <div className="loc">{file.url}</div>
+                    <div className="owner">{file.uploader}</div>
+                    <div className="size">{Math.round(file.size / 1024)}MB</div>
+                    <div className="date">
+                      {file.updatedAt
+                        .replace(/-/g, '.')
+                        .replace('T', ' ')
+                        .replace(/:[0-9]+$/, '')}
+                    </div>
+                    <div className="icon">
+                      <img src={arrow} />
+                    </div>
+                  </File>
+                );
+              })
+          : files &&
+            files.map(
+              (file: FileInfo) =>
+                file && (
+                  <File key={file.updatedAt}>
+                    <div className="name">{file.fileName}</div>
+                    <div className="loc">{file.url}</div>
+                    <div className="owner">{file.uploader}</div>
+                    <div className="size">{Math.round(file.size / 1024)}MB</div>
+                    <div className="date">
+                      {file.updatedAt
+                        .replace(/-/g, '.')
+                        .replace('T', ' ')
+                        .replace(/:[0-9]+$/, '')}
+                    </div>
+                    <div className="icon">
+                      <img src={arrow} />
+                    </div>
+                  </File>
+                ),
+            )}
+      </div>
+    </ListBox>
+  );
+};
 
 export default FileList;
